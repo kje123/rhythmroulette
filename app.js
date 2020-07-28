c763b303dfdf9c7d73a9da4b6b40ce1ff5379901
keith ellingwood <keith-3@hotmail.com>
2020-07-27 21:26:04 -0700

    added comments + documentation

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 "use strict";
 
-/* File comment */
+/**
+ * Keith Ellingwood
+ * 7/27/2020
+ * This is the backend for rhythmrou.net, which handles choosing the random songs
+ * from the database, as well as the downloads
+*/
 
 const express = require("express");
 const sqlite3 = require("sqlite3");
@@ -8,8 +13,10 @@ const sqlite = require("sqlite");
 const ytdl = require("ytdl-core");
 const app = express();
 
-app.use(express.static("public"));
-
+/**
+ * gets 3 random links from the song database and returns them
+ * in plain text, each link seperated by a new line
+*/
 app.get("/links", async function(req, res) {
     let db = await getDBConnection();
     let query = "SELECT link FROM links ORDER BY RANDOM() LIMIT 3";
@@ -22,6 +29,9 @@ app.get("/links", async function(req, res) {
     res.type("text").send(links);
 })
 
+/**
+ * gets youtube link from request and downloads it to local computer
+*/
 app.get("/download", async function(req, res) {
     let url = req.query["link"];
     let downloadName = url.substring(29, 40);
@@ -33,14 +43,16 @@ app.get("/download", async function(req, res) {
 /**
  * connects to and returns database
  * @returns {Database} the parsed database
- */
+*/
 async function getDBConnection() {
     const db = await sqlite.open({
       filename: "database.db",
       driver: sqlite3.Database
     });
     return db;
-  }
+}
+
+app.use(express.static("public"));
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT);
--- public/index.js
+++ public/index.js
@@ -1,14 +1,26 @@
 "use strict";
 
+/**
+ * Keith Ellingwood
+ * 7/27/2020
+ * This is the JavaScript that handles the behavior of rhythmrou.net
+*/
+
 (function() {
 
     window.addEventListener("load", init);
 
+    /**
+     * initializes page
+    */
     function init() {
         let randoButton = id("randomize");
         randoButton.addEventListener("click", fetchLinks);
     }
 
+    /**
+     * fetches the random songs from the server
+    */
     function fetchLinks() {
         let url = "/links"
         loadingCircle();
@@ -19,7 +31,10 @@
             .catch(videoHandler);
     }
 
-    function loadingCircle(links) {
+    /**
+     * handles adding the loading circle while the server fetches the random songs
+    */
+    function loadingCircle() {
         let circle = gen("div");
         let container = id("video-container");
         container.innerHTML = "";
@@ -27,6 +42,10 @@
         container.appendChild(circle);
     }
 
+    /**
+     * populates the webpage with the randomly chosen songs
+     * @param {String} links - all the links to the randomly chosen songs
+    */
     function addVideo(links) {
         let linksArray = links.split("\n");
         let container = id("video-container");
@@ -47,6 +66,10 @@
         }
     }
 
+    /**
+     * initiates download of given link
+     * @param {String} link - link to song to download
+    */
     function getDownload(link) {
         console.log(link);
         let url = "/download?link=" + link;
@@ -54,15 +77,17 @@
     }
 
     function videoHandler() {
-        console.log("something went wrong");
+        let container = id("video-container");
+        container.innerHTML = "";
+        container.textContent = "Something went wrong on the server, please try again later and maybe let me know on twitter @baguettegremlin"
     }
 
     /**
-    * checks status of page and returns response as text if it passes,
-    * else throws error
-    * @param {Response} response - the given page
-    * @returns {Response} if given page is valid returns itself
-    * @throws {Error} if response is not ok
+     * checks status of page and returns response as text if it passes,
+     * else throws error
+     * @param {Response} response - the given page
+     * @returns {Response} if given page is valid returns itself
+     * @throws {Error} if response is not ok
     */
     function checkStatus(response) {
         if (response.ok) {
@@ -72,25 +97,22 @@
         }
     }
 
-
     /**
-    * Returns the element that has the ID attribute with the specified value.
-    * @param {string} idName - element ID
-    * @returns {object} DOM object associated with id.
+     * Returns the element that has the ID attribute with the specified value.
+     * @param {string} idName - element ID
+     * @returns {object} DOM object associated with id.
     */
     function id(idName) {
         return document.getElementById(idName);
     }
 
     /**
-    * Returns a new ceated element of given type
-    * @param {string} elType - element type
-    * @returns {object} new element of given type
+     * Returns a new ceated element of given type
+     * @param {string} elType - element type
+     * @returns {object} new element of given type
     */
     function gen(elType) {
         return document.createElement(elType);
     }
-
-
 
 })();
